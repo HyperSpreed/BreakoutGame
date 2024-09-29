731f307608a63277b2f98674c745634aec49c34d
Sandro José Rodrigues Coelho <65609873+HyperSpreed@users.noreply.github.com>
2024-09-29 14:37:50 +0100

    Add files via upload

--- breakout/Scripts/ball.gd
+++ breakout/Scripts/ball.gd
@@ -10,23 +10,10 @@ func gameOverNow():
 	print(gameState)
 	return gameState
 
-func _ready():
-		if gameOverNow() == true:
-			var resetGame = get_node("/root/Main")
-			resetGame.game_over()
-		else:
-			var timer_label = get_node("/root/Main/TimerLabel")
-			timer_label.add_theme_font_size_override("font_size", 12)
-			#get_node("/root/Main/TimerLabel").text = "STARTING"
-			#get_node("/root/Main/TimerLabel").text = ""
-			#timer_label.text = "STARTING"
-			#timer_label.text = ""
-			await get_tree().create_timer(3).timeout
-			get_node("/root/Main/TimerLabel").text = ""
-			timer_label.add_theme_font_size_override("font_size", 52)
-			randomize()
-			VelocityBall.x = [0.45,0.5][randi()%2]
-			VelocityBall.y = [0.8,0.9][randi()%2]
+func gameStart():
+	randomize()
+	VelocityBall.x = [0.45,0.5][randi()%2]
+	VelocityBall.y = [0.8,0.9][randi()%2]
 
 func _physics_process(delta):
 	var collision_objects = move_and_collide(VelocityBall*BallSpeed*delta)
